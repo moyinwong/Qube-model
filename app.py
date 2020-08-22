c508f7220075c62ce3f7d1c28415ad038ed01ea7
Kelvin Wong <cheukyinwong1995@gmail.com>
2020-08-22 17:26:57 +0800

    trial

--- app.py
+++ app.py
@@ -11,20 +11,25 @@ app = Flask(__name__)
 def run():
     return "<h1 style='color:blue'>Hello There!</h1>"
 
-@app.route("/solve",methods=['POST'])
-def predict():
+@app.route("/solve")
+def run():
+    return "<h1 style='color:blue'>Hello There!</h1>"
 
-    content = request.get_json()
-    target_cube = turn(content)
-    if type(target_cube) == "str":
-        return json.dumps([False, "Invalid input"])
-    if target_cube.is_valid() == False:
-        return json.dumps([False, "Invalid input"])
 
-    is_solved, actions = solve(target_cube)
-    results = [is_solved, actions]
+# @app.route("/solve",methods=['POST'])
+# def predict():
 
-    return json.dumps(results)
+#     content = request.get_json()
+#     target_cube = turn(content)
+#     if type(target_cube) == "str":
+#         return json.dumps([False, "Invalid input"])
+#     if target_cube.is_valid() == False:
+#         return json.dumps([False, "Invalid input"])
+
+#     is_solved, actions = solve(target_cube)
+#     results = [is_solved, actions]
+
+#     return json.dumps(results)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
