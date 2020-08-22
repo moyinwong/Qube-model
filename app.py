a1311ba7513132192af08d8a2d3db5e6e4263a51
Kelvin Wong <cheukyinwong1995@gmail.com>
2020-08-22 17:29:45 +0800

    merge

--- app.py
+++ app.py
@@ -16,20 +16,20 @@ def run():
     return "<h1 style='color:blue'>Hello There!</h1>"
 
 
-# @app.route("/solve",methods=['POST'])
-# def predict():
+@app.route("/solve",methods=['POST'])
+def predict():
 
-#     content = request.get_json()
-#     target_cube = turn(content)
-#     if type(target_cube) == "str":
-#         return json.dumps([False, "Invalid input"])
-#     if target_cube.is_valid() == False:
-#         return json.dumps([False, "Invalid input"])
+    content = request.get_json()
+    target_cube = turn(content)
+    if type(target_cube) == "str":
+        return json.dumps([False, "Invalid input"])
+    if target_cube.is_valid() == False:
+        return json.dumps([False, "Invalid input"])
 
-#     is_solved, actions = solve(target_cube)
-#     results = [is_solved, actions]
+    is_solved, actions = solve(target_cube)
+    results = [is_solved, actions]
 
-#     return json.dumps(results)
+    return json.dumps(results)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
